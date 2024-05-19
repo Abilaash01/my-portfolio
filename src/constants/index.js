269562700a26b9863517fee2e816b82427342c18
Abilaash <abilaash2001@gmail.com>
2024-05-18 20:45:41 -0400

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,255 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    distiller,
+    sfl,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Full Stack Developer - Co-op",
+      company_name: "DistillerSR",
+      icon: distiller,
+      iconBg: "#E6DEDD",
+      date: "Sep 2022 - Apr 2023",
+      points: [
+        "Enhanced user capabilities by implementing advanced features for intricate data analytics through JavaScript, PHP, and React.",
+        "Coordinated closely with various teams to plan and execute the transition from the existing technology stack to React.",
+        "Spearheaded a project to redesign a critical forms page, employing PHP and JavaScript, enabling users to seamlessly add custom date formats.",
+        "Implemented a dynamic feature allowing the format to automatically adjust to future changes made by admins, enhancing flexibility and user customization.",
+      ],
+    },
+    {
+      title: "Quality Control Engineer - Co-op",
+      company_name: "DistillerSR",
+      icon: distiller,
+      iconBg: "#E6DEDD",
+      date: "Jan 2022 - Apr 2022",
+      points: [
+        "Created test scenarios, test protocols and automation test scripts using Cypress, Protractor, Selenium and other automated testing tools.",
+        "Utilized Cypress for streamlined test automation, reducing testing time on production release by over 60%.",
+        "Investigated and debugged test failures, updating tests or reporting bugs as necessary, and provided test coverage analysis based on automation results.",
+        "Applied Postman for API testing, and collaborated with cross-functional teams to ensure seamless integration.",
+      ],
+    },
+    {
+      title: "Logistics Coordinator - Co-op",
+      company_name: "Wipebook Corporation",
+      icon: sfl,
+      iconBg: "#383E56",
+      date: "May 2021 - Aug 2021",
+      points: [
+        "Streamlined workflows to enhance efficiency, reducing order processing time by 30%.",
+        "Provided exceptional customer support, addressing inquiries and resolving issues promptly.",
+        "Established strong partnerships with carriers, improving delivery reliability and tracking accuracy.",
+        "Oversaw the creation and processing of outgoing invoices, streamlining the billing process and reducing errors by 15%.",
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company_name: "Software For Love",
+      icon: sfl,
+      iconBg: "#383E56",
+      date: "Sep 2020 - Jan 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
