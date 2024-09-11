959d8c678a546a1049423e239bbb0c2c0b81fc2d
Abilaash <abilaash2001@gmail.com>
2024-09-11 17:28:49 -0400

    render

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,22 +1,20 @@
-import { useState, useRef, Suspense } from 'react'
-
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Points, PointMaterial, Preload } from '@react-three/drei'
-
-import * as random from 'maath/random/dist/maath-random.esm'
+import { useState, useRef, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import { useInView } from 'react-intersection-observer';
+import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
-  const ref = useRef()
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 15
-    ref.current.rotation.y -= delta / 20
-  })
-  
+    ref.current.rotation.x -= delta / 15;
+    ref.current.rotation.y -= delta / 20;
+  });
+
   return (
     <group>
-
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
@@ -27,21 +25,27 @@ const Stars = (props) => {
         />
       </Points>
     </group>
-  )
-}
+  );
+};
 
 const StarsCanvas = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Load the starfield once when it comes into view
+    threshold: 0.1, // Adjust the threshold as needed
+  });
+
   return (
-    <div className='w-full h-full absolute inset-0 z-[-1]'>
-      <Canvas camera={{ position: [0, 0, 1] }}>
-        <Suspense fallback={null}>
-          <Stars />
-        </Suspense>
-
-        <Preload all />
-      </Canvas>
+    <div ref={ref} className='w-full h-full absolute inset-0 z-[-1]'>
+      {inView && (
+        <Canvas camera={{ position: [0, 0, 1] }}>
+          <Suspense fallback={null}>
+            <Stars />
+          </Suspense>
+          <Preload all />
+        </Canvas>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default StarsCanvas
+export default StarsCanvas;
