308782f0acd55235c7d2b6dde2159dca5aed7b24
Abilaash <abilaash2001@gmail.com>
2025-08-19 15:55:22 -0400

    changing aspiring to experienced in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Abilaash</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            An aspiring software developer
+            An experienced software developer
           </p>
         </div>
       </div>
